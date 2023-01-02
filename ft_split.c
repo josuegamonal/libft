a7c3f0f0c9ffa1bdca85457a077e54a6ef58daa8
josuegamonal <69472426+josuegamonal@users.noreply.github.com>
2023-01-02 20:10:08 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: josue <josue@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 08:43:39 by jgamonal          #+#    #+#             */
-/*   Updated: 2022/12/19 19:42:46 by josue            ###   ########.fr       */
+/*   Updated: 2023/01/02 20:09:46 by josue            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*static int	count_words(const char *str, char c)
+static int	count_words(const char *str, char c)
 {
 	int	i;
 	int	trigger;
@@ -40,10 +40,23 @@ static char	*word_dup(const char *str, int start, int finish)
 
 	i = 0;
 	word = (char *)malloc((finish - start + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
 	while (start < finish)
 		word[i++] = str[start++];
 	word[i] = '\0';
 	return (word);
+}
+
+static char	**ft_free(char **split, int j)
+{
+	while (j >= 0)
+	{
+		free(split[j]);
+		j--;
+	}
+	free(split);
+	return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
@@ -53,11 +66,9 @@ char	**ft_split(char const *s, char c)
 	int		index;
 	char	**split;
 
-	if (!(s))
-		return (NULL);
 	split = (char **) malloc((count_words(s, c) + 1) * sizeof(char *));
-	if (!s || !split)
-		return (0);
+	if (!split)
+		return (NULL);
 	i = -1;
 	j = 0;
 	index = -1;
@@ -68,95 +79,11 @@ char	**ft_split(char const *s, char c)
 		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
 		{
 			split[j++] = word_dup(s, index, i);
+			if (*split == NULL)
+				ft_free(split, j);
 			index = -1;
 		}
 	}
 	split[j] = 0;
 	return (split);
-}*/
-
-static int	tot_len(const char *s, char c)
-{
-	int	len;
-
-	len = 0;
-	while (s[len] != '\0' && s[len] != c)
-		len++;
-	return (len);
-}
-
-int	ft_word_count(const char *s, char c)
-{
-	int	i;
-	int	count;
-
-	count = 0;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		while (s[i] != '\0' && s[i] == c)
-			i++;
-		if (s[i] != '\0' && s[i] != c)
-			count++;
-		while (s[i] != '\0' && s[i] != c)
-			i++;
-	}
-	return (count);
-}
-
-char	*ft_strddup(const char *s, char c)
-{
-	int		i;
-	char	*str;
-
-	i = 0;
-	str = ft_calloc(tot_len(s, c) + 1, sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	while (s[i] != '\0' && s[i] != c)
-	{
-		str[i] = s[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-static char	**ft_free(char **tab, int i)
-{
-	while (i >= 0)
-	{
-		free(tab[i]);
-		i--;
-	}
-	free(tab);
-	return (NULL);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	size_t	i;
-	char	**tab;
-
-	i = 0;
-	if (s == NULL)
-		return (NULL);
-	tab = ft_calloc(ft_word_count(s, c) + 1, sizeof(char *));
-	if (tab == NULL)
-		return (NULL);
-	while (*s && ft_word_count(s, c))
-	{
-		while (*s != '\0' && *s == c)
-			s++;
-		if (*s != '\0' && *s != c)
-		{
-			tab[i] = ft_strddup(s, c);
-			if (tab[i] == NULL)
-				return (ft_free(tab, i));
-			i++;
-		}
-		while (*s != '\0' && *s != c)
-			s++;
-	}
-	return (tab);
 }
